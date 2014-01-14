ab57451bf0d0f39c4a42a2c71be2308d631c86af
Eko Tristiyono <eko@xintesa.com>
2014-01-14 18:23:51 +0700

    add Config Cosmess plugin

---
+++ Config/cosmess.default.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Configuration Cosmess plugin
+ */
+
+$config = array(
+	'Cosmess.Params.flash_type' => '',
+
+	'Cosmess.Class.foundation.success' => '',
+	'Cosmess.Class.foundation.error' => '',
+
+	'Cosmess.Class.bootstrap.success' => '',
+	'Cosmess.Class.bootstrap.error' => ''
+);
